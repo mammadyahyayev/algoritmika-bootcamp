ded43465df01589b6ddf6c76777b61466dfe59e8
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-13 17:04:58 +0400

    adding and sorting functionalities created for todos

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // TODO: When user close the window, store them in local storage
 // TODO: When user enter the page load all of his todos
 
+// DOM selectors
+const todoListElement = document.getElementById("todos");
+const todoElement = document.querySelectorAll(".todo");
+const insertBtnElement = document.getElementById("insertBtn");
+const todoInput = document.getElementById("todo-input");
+const toolTipElement = document.getElementById("tooltip-box");
+const sortIconElement = document.getElementById("sort-icon");
+
 let todos = [];
 
 class Todo {
@@ -8,16 +16,16 @@ class Todo {
   #id;
   #date;
 
-  constructor(todo) {
+  constructor(todo = "") {
     this.#todo = todo;
     this.#date = new Date();
+    this.#id = TodoHelper.generateId();
   }
 
   insertTodo() {
-    this.#id = TodoHelper.generateId();
     const todoObj = {
       id: this.#id,
-      todo: this.#todo,
+      todo: this.#todo.trim(),
       date: this.#date,
     };
     todos.push(todoObj);
@@ -30,7 +38,40 @@ class Todo {
     todos = [...copiedTodos];
   }
 
-  sortAscTodos() {
+  createHtmlTodoElement(todo) {
+    console.log(todo);
+    const todoDiv = DomHelper.createElement("div", {
+      className: "todo",
+      id: `todo-${todo.#id}`,
+    });
+    const todoInput = DomHelper.createElement("input", {
+      type: "text",
+      value: todo.#todo.trim(),
+    });
+    const wrongIconDiv = DomHelper.createElement("div", {
+      className: "wrong-icon",
+    });
+    const wrongSign = DomHelper.createElement("p", { textContent: "X" });
+
+    wrongIconDiv.appendChild(wrongSign);
+    todoDiv.appendChild(todoInput);
+    todoDiv.appendChild(wrongIconDiv);
+    todoListElement.appendChild(todoDiv);
+
+    return todoDiv;
+  }
+}
+
+class TodoHelper {
+  static generateId() {
+    return Math.floor(Math.random() * 100_000_000) + 1;
+  }
+
+  static parseDate(date) {
+    // parse default date format into dd:MM:yyyy HH:mm:ss
+  }
+
+  static sortAscTodos() {
     const copiedTodos = [...todos];
 
     copiedTodos.sort((firstTodo, secondTodo) => {
@@ -44,7 +85,7 @@ class Todo {
     todos = [...copiedTodos];
   }
 
-  sortDescTodos() {
+  static sortDescTodos() {
     const copiedTodos = [...todos];
 
     copiedTodos.sort((firstTodo, secondTodo) => {
@@ -57,14 +98,81 @@ class Todo {
 
     todos = [...copiedTodos];
   }
-}
 
-class TodoHelper {
-  static generateId() {
-    return Math.floor(Math.random() * 100_000_000) + 1;
+  static loadAllTodos() {
+    DomHelper.removeAllChildElements(todoListElement);
+
+    todos.forEach((todo) => {
+      const todoDiv = DomHelper.createElement("div", {
+        className: "todo",
+        id: `todo-${todo.id}`,
+      });
+      const todoInput = DomHelper.createElement("input", {
+        type: "text",
+        value: todo.todo,
+      });
+      const wrongIconDiv = DomHelper.createElement("div", {
+        className: "wrong-icon",
+      });
+      const wrongSign = DomHelper.createElement("p", { textContent: "X" });
+
+      wrongIconDiv.appendChild(wrongSign);
+
+      todoDiv.appendChild(todoInput);
+      todoDiv.appendChild(wrongIconDiv);
+
+      todoListElement.appendChild(todoDiv);
+    });
+
+    todoListElement.scrollTo({ top: 0, behavior: "smooth" });
   }
 
-  static parseDate(date) {
-    // parse default date format into dd:MM:yyyy HH:mm:ss
+  static addTodoElement() {
+    if (DomHelper.hasElementValue(todoInput)) {
+      const todo = new Todo(todoInput.value);
+      todo.insertTodo();
+      const newTodoElement = todo.createHtmlTodoElement(todo);
+      document.getElementById(newTodoElement.id).scrollIntoView();
+      todoInput.value = "";
+    } else {
+      toolTipElement.classList.add("visible");
+
+      setTimeout(() => {
+        toolTipElement.classList.remove("visible");
+      }, 2000);
+    }
   }
 }
+
+class DomHelper {
+  static createElement(elementType = "div", properties) {
+    const element = document.createElement(elementType);
+    Object.assign(element, properties);
+    return element;
+  }
+
+  static hasElementValue(element) {
+    if (element.value.trim() === "" || element.value.trim() === null)
+      return false;
+
+    return true;
+  }
+
+  static removeAllChildElements(parentElement) {
+    while (parentElement.firstChild) {
+      parentElement.firstChild.remove();
+    }
+  }
+}
+
+// events
+insertBtnElement.addEventListener("click", TodoHelper.addTodoElement);
+
+sortIconElement.addEventListener("click", () => {
+  sortIconElement.classList.toggle("rotate");
+
+  if (sortIconElement.classList.contains("rotate")) TodoHelper.sortDescTodos();
+  else TodoHelper.sortAscTodos();
+
+  TodoHelper.loadAllTodos();
+});
