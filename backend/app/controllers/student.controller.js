b63c34bf49e10c278c74aa46ec10210b80a4c8f3
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-01 11:20:07 +0400

    student controller created

---
+++ backend/app/controllers/student.controller.js
@@ -0,0 +1,15 @@
+const db = require("../models");
+const Student = db.students;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {};
+
+exports.findAll = (req, res) => {};
+
+exports.findOne = (req, res) => {};
+
+exports.update = (req, res) => {};
+
+exports.delete = (req, res) => {};
+
+exports.deleteAll = (req, res) => {};
