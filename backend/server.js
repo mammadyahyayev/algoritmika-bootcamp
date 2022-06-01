909cb07d86d1025e2d13bcef0e6111c6686de51f
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-01 11:48:55 +0400

    api routes created

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
-
 const db = require('./app/models')
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
 
+require("./app/routes/student.routes")(app);
 
 const PORT = process.env.PORT || 9000;
 
