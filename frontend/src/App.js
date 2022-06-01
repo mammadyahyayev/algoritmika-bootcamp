760bad52c04c174330cc3573b4d01fc371822e84
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-01 22:42:22 +0400

    react routing configured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import MainPage from "./pages/MainPage";
+import StudentsPage from "./pages/StudentsPage";
+import StudentEditor from "./components/student/StudentEditor";
+import Student from "./components/student/Student";
 
 function App() {
   return (
     <div className="App">
-      <h1>Student Management</h1>
+      <Routes>
+        <Route path="/" exact element={<MainPage />} />
+        <Route path="/students" exact element={<StudentsPage />} />
+        <Route path="/add" exact element={<StudentEditor />} />
+        <Route path="/students/:id" element={<Student />} />
+      </Routes>
     </div>
   );
 }
