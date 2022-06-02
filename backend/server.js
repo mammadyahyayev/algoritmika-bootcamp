20a9cad7fb242cf1077c011b0312a14808c7107c
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-02 20:19:44 +0400

    changing cross origin port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:4000",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
