e4b38a50475b2260631368b66cbf0cf0774f51a7
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-02 20:20:28 +0400

    students fetched from server

--- frontend/src/components/student/Student.jsx
+++ frontend/src/components/student/Student.jsx
@@ -4,16 +4,16 @@ import EditIcon from "../../assets/images/edit.png";
 import DeleteIcon from "../../assets/images/delete.png";
 import "./Student.css";
 
-const Student = () => {
+const Student = ({student}) => {
   return (
     <div className="student">
       <div className="student-info">
-        <h5>Samir Samirov</h5>
-        <p>18.05.2000</p>
-        <p className="email">samirsamirov@gmail.com</p>
+        <h5>{student.name + " " + student.surname}</h5>
+        <p>{student.birthDate}</p>
+        <p className="email">{student.email}</p>
       </div>
       <div className="student-action">
-        <Link to={"/edit/1"} className="nav-link edit-icon">
+        <Link to={`/edit/${student.id}`} className="nav-link edit-icon">
           <img src={EditIcon} alt="edit icon" />
         </Link>
         <img src={DeleteIcon} alt="delete icon" />
--- frontend/src/components/student/Students.jsx
+++ frontend/src/components/student/Students.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import Student from "./Student";
 
 const Students = () => {
+  const [students, setStudents] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:9000/api/students")
+      .then((response) => response.json())
+      .then((data) => setStudents(data));
+  }, []);
+
   return (
     <div className="d-flex align-items-center flex-wrap justify-content-center mt-3">
-      <Student />
-      <Student />
-      <Student />
-      <Student />
-      <Student />
-      <Student />
-      <Student />
-      <Student />
+      {students.length !== 0 ? (
+        students.map((student) => (
+          <Student key={student.id} student={student} />
+        ))
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
