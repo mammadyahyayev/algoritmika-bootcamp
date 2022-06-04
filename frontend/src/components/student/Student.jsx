fa16f70ef123ac73cf12a5020bb6920421829a45
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-04 11:16:34 +0400

    adding tooltip to show fullname if it is long

--- frontend/src/components/student/Student.jsx
+++ frontend/src/components/student/Student.jsx
@@ -4,11 +4,14 @@ import EditIcon from "../../assets/images/edit.png";
 import DeleteIcon from "../../assets/images/delete.png";
 import "./Student.css";
 
-const Student = ({student}) => {
+const Student = ({ student, onRemoveStudent }) => {
+  const fullname = student.name + " " + student.surname;
   return (
     <div className="student">
       <div className="student-info">
-        <h5>{student.name + " " + student.surname}</h5>
+        <h5 className="fullname" title={fullname}>
+          {fullname}
+        </h5>
         <p>{student.birthDate}</p>
         <p className="email">{student.email}</p>
       </div>
@@ -16,7 +19,11 @@ const Student = ({student}) => {
         <Link to={`/edit/${student.id}`} className="nav-link edit-icon">
           <img src={EditIcon} alt="edit icon" />
         </Link>
-        <img src={DeleteIcon} alt="delete icon" />
+        <img
+          src={DeleteIcon}
+          alt="delete icon"
+          onClick={() => onRemoveStudent(student.id)}
+        />
       </div>
     </div>
   );
